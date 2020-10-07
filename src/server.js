70c500a97ee73d1a0a7eef56907532987d175301
jjf7 <jfsistemas.contacto@gmail.com>
2020-10-06 21:26:56 -0300

    my first upload

---
+++ src/server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const morgan = require('morgan')
+
+const app = express()
+
+app.use(morgan('dev'))
+app.use(express.urlencoded({extended:false}))
+app.use(express.json())
+
+app.use('/', (req,res)=>{
+    res.json({message:'Welcome to my api.'})
+})
+
+module.exports = app 
